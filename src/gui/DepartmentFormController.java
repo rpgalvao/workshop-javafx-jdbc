0f2a7ca483d1b1848c170713a122df10ea7990f6
Renato Galvao <renatopgalvao@gmail.com>
2020-01-08 11:50:29 -0300

    Remove department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -87,7 +87,7 @@ public class DepartmentFormController implements Initializable {
 	private Department getFormData() {
 		Department obj = new Department();
 		ValidationException exception = new ValidationException("Validation error");
-		obj.setId(Utils.tryParseToInt(txtId.getId()));
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
 		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
 			exception.addError("name", "Field can't be blank");
 		}
